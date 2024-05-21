546aff4e8093b77d18e111010eea7761ca534a72
davMac606 <worksender053@gmail.com>
2024-05-21 20:46:36 -0300

    working on expand

--- list7.py
+++ list7.py
@@ -1,5 +1,6 @@
 import os
 def ex2():
+    print("Exercise 2: Remove unnecessary spaces")
     ans = input("Do you want the version using 'strip' method? (y/n)")
     match ans:
         case 'y':
@@ -8,3 +9,17 @@ def ex2():
         case 'n':
             s = input("Enter a string: ")
             print(s.replace(" ", ""))
+            
+def ex3(): 
+    print("Exercise 3: Expand dashes")
+    s = input("Enter a string: ")
+    ind = s.find("-")
+    prev = s[ind-1]
+    nex = s[ind+1]
+    letter = ord(prev)
+    while letter <= ord(nex):
+        print(chr(letter), end="")
+        letter += 1
+    
+    
+    
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from list7 import ex2
+from list7 import ex2, ex3
 print("Welcome!")
 print("List 7: String exercises")
 print("List 8: Tuple exercises")
@@ -6,7 +6,14 @@ print("List 9: Dictionary exercises")
 ans = int(input("Select a list: "))
 match ans:
     case 7:
+        print("List 7: String exercises")
+        print("[2] Remove unnecessary spaces")
+        print("[3] Expand dashes")
         ans = int(input("Select an exercise: "))
         match ans:
             case 2:
                 ex2()
+            case 3:
+                ex3()
+            case _:
+                print("Invalid exercise")
